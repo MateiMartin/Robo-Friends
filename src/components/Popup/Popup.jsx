dc73ed00416326ca2d9bd9cc743fdacfb7e2c3c1
Martin Matei <martin.matei@gmail.com>
2023-06-29 21:15:12 +0300

    finished the popup visual

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -7,14 +7,16 @@ const Popup = ({ clicked }) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
+    let height = id ? '400px' : '300px';
+
     return (
         <>
             {clicked && (
-                <div className="Popup bg-light-green dib br3 pa3 ma2 bw2 shadow-5">
-                    <img src={''} />
-                    <div className="tc pt6">
+                <div className="Popup bg-light-green dib br3 pa3 ma2 bw2 shadow-5 flex flex-column" style={{height:height}}>
+                    {id && <img src={`https://robohash.org/${id}`} className="poza" alt="robot"/>}
+                    <div className="tc">
                         <div className="flex justify-center flex-column">
-                            <input type="number" placeholder="id..." className="mt2 w-150 h2 f6" />
+                            <input type="number" placeholder="Id..." className="mt2 w-150 h2 f6" onChange={(e) => setId(e.target.value)} />
                             <input placeholder="Name..." className="mt2 w-150 h2 f6" />
                             <input placeholder="Email..." className="mt2 w-150 h2 f6" />
                         </div>
